916f181d58e5e9d5c4a12dd70941243572eb5013
Lucas Cavalheiro <luccribeiro53@gmail.com>
2023-09-26 10:53:41 -0300

    fix: remove dependencies not used

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,20 +1,16 @@
 import { AntDesignProvider } from "./AntDesignProvider";
-import { NextAuthProvider } from "./NextAuthProvider";
 import { ReactReduxProvider } from "./ReactReduxProvider";
 
 interface Props {
-    pageProps: any;
     children: React.ReactNode
 }
 
-export const Providers = ({ pageProps: { session }, children }: Props) => {
+export const Providers = ({ children }: Props) => {
     return (
         <ReactReduxProvider>
-            <NextAuthProvider session={session}>
-                <AntDesignProvider>
-                    {children}
-                </AntDesignProvider>
-            </NextAuthProvider>
+            <AntDesignProvider>
+                {children}
+            </AntDesignProvider>
         </ReactReduxProvider>
     )
 }
